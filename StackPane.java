4c6296805498864e611239fdc4c58112bbf32dfc
masrie03 <116857476+masrie03@users.noreply.github.com>
2022-11-28 13:38:50 +0800

    Add files via upload

---
+++ StackPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test1;
+
+/**
+ *
+ * @author masri
+ */
+class StackPane {
+
+    Object getChildren() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ label.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test1;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author masri
+ */
+class label {
+
+    static void setIcon(ImageIcon scaledIcon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
